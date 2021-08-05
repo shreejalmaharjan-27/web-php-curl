52db0aec781a8293f4f3527970b1605f596acd44
shreejal maharjan <shreejal@rafled.com>
2021-08-05 09:06:30 +0545

    :tada: fixed soemthing i guess

--- webcurl.php
+++ webcurl.php
@@ -8,7 +8,7 @@ $actions = array(      // Array of actions
     'v4' => '4',
     'v6' => '6',
     'getheader' => 'I',
-    'verbose' => 'v',
+    'verbose' => 'v'
 );
 $domaina = $_POST['domain'];
 $ipva = $_POST['curl1'];
@@ -23,15 +23,14 @@ if (isset($_POST['curl1']))
     
     // disallow connection if domain has other than letters, numbers, and hyphens
     if (preg_match("/^[.A-Za-z0-9-]+$/", $domain)) {
-        $nth = "";
         if(isset($_POST['curl1']) && array_key_exists($_POST['curl1'], $actions)) {
          if(isset($_POST['curl2']) && array_key_exists($_POST['curl2'], $actions)) { 
          if(isset($_POST['curl3']) && array_key_exists($_POST['curl3'], $actions)) {            
                 //connect ipv4/v6
 
-                $ipv = $actions[$_POST['curl1']];
-                $ipv1 = $actions[$_POST['curl2']];
-                $ipv2 = $actions[$_POST['curl3']];
+                $ipv = $actions[$_POST['curl1'] ?? ""];
+                $ipv1 = $actions[$_POST['curl2'] ?? ""];
+                $ipv2 = $actions[$_POST['curl3'] ?? ""];
                 echo "<strong>Command Sample: curl -$ipv$ipv1$ipv2 $domain</strong>";
                 #echo "<code> $domain $ipv $ipv1 $ipv2</code>";
                 
