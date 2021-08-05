a6a7a294d158a178855cb1391780f824e9438340
shreejal maharjan <shreejal@rafled.com>
2021-08-05 07:02:42 +0545

    Initial Test :beers:

--- index.php
+++ index.php
@@ -4,5 +4,16 @@
 <title>cURL websites over web</title>
 </head>
 <body>
+<form method="post" enctype="multipart/form-data" action="webcurl.php" name="form">  
+<input required name="domain" placeholder="enter domain name">
+<select name="curl1">
+    <option value="" disabled selected>Choose connect method</option>
+    <option value="v4">IPV4</option>
+    <option value="v6">IPV6</option>
+</select>
+<input type="checkbox" value="getheader" name="curl1"><label>Get Head (-I)</label>
+<input type="checkbox" value="verbose" name="curl1"><label>Get Verbose Output (-v)</label>
+ <input name="submit" type="submit">
+</form>
 </body>
 </html>
---
+++ webcurl.php
@@ -0,0 +1,45 @@
+<?php
+//get domain query
+$actions = array(      // Array of actions
+    'v4' => '-4',
+    'v6' => '-6',
+    'getheader' => '-I',
+    'verbose' => '-v'
+);
+if (isset($_POST['curl1']))
+{   
+    //get domain from input
+
+    $domain = $_POST['domain'];
+    
+    // disallow connection if domain has other than letters, numbers, and hyphens
+    if (preg_match("/^[:.A-Za-z0-9-]+$/", $domain)) {
+        if(isset($_POST['curl1']) && array_key_exists($_POST['curl1'], $actions)) {
+         if(isset($_POST['ipversion1']) && array_key_exists($_POST['ipversion1'], $actions)) {
+            if(isset($_POST['ipversion1']) && array_key_exists($_POST['ipversion2'], $actions)) {
+            
+                //connect ipv4/v6
+                $ipv = $actions[$_POST['curl1']];
+                #$ipv1 = $actions[$_POST['ipversion1']];
+               # $ipv2 = $actions[$_POST['ipversion2']];
+
+                echo "<strong>Command Sample: curl </strong>";
+                #echo "<code> $domain $ipv $ipv1 $ipv2</code>";
+                echo "<code> $domain $ipv</code>";
+                
+                //execute shell command
+                #$shellexec = shell_exec("dig $domain $ipv $ipv1 $ipv2");
+                $shellexec = shell_exec("curl $ipv $domain");
+
+                echo '<pre>'.$shellexec.'</pre>';
+        }
+            } else {
+            die('Unknown Message');  // Otherwise, display and error and end execution
+            }
+        }
+    } else {
+    // display error
+    echo 'Invalid domain name provided!';
+    }
+}
+?>
